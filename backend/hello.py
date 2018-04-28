46c21809f349b9bfbd61146c774bad4ceed8a840
Mohit Ahuja <ahujamoh@gmail.com>
2018-04-28 14:32:32 +0200

    adding summary parameter text, changing from constant string

--- backend/hello.py
+++ backend/hello.py
@@ -6,31 +6,34 @@ Author: Ian Huston
 License: See LICENSE.txt
 
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
 
 # Get port from environment variable or choose 9099 as local default
 port = int(os.getenv("PORT", 9099))
+precire_api_key = str(os.getenv("PRECIRE_API", ""))
 
 @app.route('/')
 def hello_world():
     return 'Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
 
 
-@app.route('/precire')
+#summari
+@app.route('/summary')
 def precire():
+    assert precire_api_key != ""
 
     import requests
     headers = {
-        'Ocp-Apim-Subscription-Key': '$$$$',
+        'Ocp-Apim-Subscription-Key': precire_api_key,
         'Content-Type': 'application/json',
         'Content-Language': 'de',
     }
     body = {
             'document': {
-                'text': ('Das PRECIRE Team wünscht viel Erfolg und vor allem viel Spaß beim Hack-It-Over 2018.'),
+                'text': request.args.get('text'),
                 'type': 'default',
     },
     'results': ['friendly'],
@@ -39,6 +42,7 @@ def precire():
     response = requests.post('https://api.precire.ai/v0.9/',
                         json=body,
                         headers=headers)
+
     assert response.status_code == 200
     data = response.json()
     return jsonify(data)
